ab5f64b7948204c211eae93d58877799dafbb69d
EugeneTsydenov <ecydenov110@gmail.com>
2026-05-30 13:25:36 +0700

    refactor: remove adding and removing dublicates to index

--- src/commands/add.c
+++ src/commands/add.c
@@ -3,7 +3,26 @@
 #include "repo/repo.h"
 #include "result/result.h"
 #include "util/util.h"
+#include <stdlib.h>
 #include <string.h>
+
+int is_already_added(const char *path) {
+    int count = 0;
+    IndexRow *rows = repo_read_index(&count);
+
+    int found = 0;
+    for (int i = count - 1; i >= 0; i--) {
+        if (strcmp(rows[i].path, path) == 0) {
+            if (rows[i].mod == 'a') {
+                found = 1;
+            }
+            break;
+        }
+    }
+
+    free(rows);
+    return found;
+}
 
 Result add(char *file_name) {
     if (!repo_exists_local()) {
@@ -18,6 +37,10 @@ Result add(char *file_name) {
 
     if (strncmp(normalized, MINIGIT_DIR, 8) == 0) {
         return RESERVED_MINIGIT_PATH;
+    }
+
+    if (is_already_added(normalized)) {
+        return OK;
     }
 
     repo_write_index('a', normalized);
